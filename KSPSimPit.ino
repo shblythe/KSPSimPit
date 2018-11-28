e0463d46f296521e35c0c619b874802955240b23
Stephen Blythe <stephen@urwick.co.uk>
2018-11-28 23:07:45 +0000

    The basics.  Staging works, and displays VVI.

--- KSPSimPit.ino
+++ KSPSimPit.ino
@@ -1,10 +1,12 @@
-#include "KerbalSimpit.h"
 #include <LiquidCrystal.h>
 #include <Button.h>
 
-#define LCD 0
+#include <KerbalSimpit.h>
+#include <PayloadStructs.h>
 
-Button stageBtn(11);
+#define LCD 1
+
+Button stageBtn(6);
 
 #if LCD
 // initialize the library by associating any needed LCD interface pin
@@ -15,13 +17,92 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 KerbalSimpit mySimpit(Serial);
 
+float vvi=0;
+int msgCount=0;
+
+const char mult_chars[]="afpnum kMGTPE";
+const int default_mult=6;
+
+//convert a value to an integer with the specified number of digits, decimal places, and a multiplier, e.g. M, k etc.
+//value - the value to convert
+//dp - the number of decimal places
+//str - the destination string
+//len - the total length of the string, excluding '\0'
+void dispValue(float value, int dp, char *str, int len, bool dashneg, bool scale)
+{
+  int numlen=len;
+  if (scale)
+    numlen-=1; // subtract 1 for the multiplier
+  if (value<0 && dashneg)
+  {
+    for (int i=0; i<len; i++)
+      str[i]='-';
+    str[len]='\0';
+    return;
+  }
+  else
+    numlen=len-1; // subtract 1 for the sign
+  int mult=default_mult;  // default multiplier is none (1)
+  if (dp>0)
+    numlen-=(dp+1);  // subtract the number of DPs, plus the space for the DP itself
+  int maxnum=pow(10,numlen);
+  float minnum=powf(10,-dp);
+  if (scale)
+  {
+    while (fabsf(value)>maxnum)
+    {
+      value/=1000.0;
+      mult+=1;
+    }
+    while (value!=0.0 && fabsf(value)<minnum)
+    {
+      value*=1000.0;
+      mult-=1;
+    }
+  }
+  if (dp>0)
+  {
+    dtostrf(value,numlen+dp+1,dp,str);
+    if (scale)
+      str[numlen+dp+1]=mult_chars[mult];
+  }
+  else    
+  {
+    char fmt[80];
+    if (scale)
+    {
+      snprintf(fmt,80,"%%%dd%%c",numlen);
+      snprintf(str,len+1,fmt,(int)value,mult_chars[mult]);
+    }
+    else
+    {
+      snprintf(fmt,80,"%%%dd",numlen);
+      snprintf(str,len+1,fmt,(int)value);
+    }
+  }
+}
+
+void dispTime(int value, char *str, int len)
+{
+  snprintf(str,len+1,"%02d:%02d",value/60,value%60);
+}
+
+void callbackHandler(byte msgType, byte msg[], byte msgSize)
+{
+  msgCount++;
+  switch (msgType) {
+    case VELOCITY_MESSAGE:
+      if (msgSize==sizeof(velocityMessage))
+      {
+        velocityMessage vel;
+        vel=parseVelocity(msg);
+        vvi=vel.vertical;        
+      }
+  }
+}
+
 void setup() {
-#if LCD
-  lcd.begin(16,2);
-  lcd.clear();
-  lcd.print("UPLOAD WINDOW");
-#endif
-  delay(2000);  // Wait two seconds to allow upload from IDE before we start
+  stageBtn.begin();
 
 #if LCD
   lcd.clear();
@@ -30,38 +111,65 @@ void setup() {
   // Open the serial connection.
   Serial.begin(38400);
 
+#if !LCD
   // Set initial pin states, and turn on the LED
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+#endif
   int mode=LOW;
 
   // This loop continually attempts to handshake with the plugin.
   // It will keep retrying until it gets a successful handshake.
   while (!mySimpit.init()) {
     delay(100);
+#if !LCD
     digitalWrite(LED_BUILTIN,mode);
+#endif
     if (mode==LOW)
       mode=HIGH;
     else
       mode=LOW;
   }
   
-  // Turn off the built-in LED to indicate handshaking is complete.
-  digitalWrite(LED_BUILTIN, LOW);
 #if LCD
   lcd.clear();
   lcd.print("CONNECTED");
+#else
+  // Turn off the built-in LED to indicate handshaking is complete.
+  digitalWrite(LED_BUILTIN, LOW);
 #endif
+
+  mySimpit.inboundHandler(callbackHandler);
+  mySimpit.registerChannel(VELOCITY_MESSAGE);
 }
 
 void loop() {
+  char line1[17];
+  char line2[17];
+  mySimpit.update();  // check for new serial message
   // put your main code here, to run repeatedly:
   if (stageBtn.pressed())
   {
+#if !LCD
     digitalWrite(LED_BUILTIN,HIGH);
     delay(100);
     digitalWrite(LED_BUILTIN,LOW);
+#endif
     mySimpit.activateAction(STAGE_ACTION);
   }
-  delay(1);
+  {
+    char vvi_string[6];
+    dispValue(vvi,0,vvi_string,5,false,false);
+    snprintf(line1,17,"VVI%5s %5d   ",vvi_string,msgCount);
+    snprintf(line2,17,"                ");
+  }
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(line1);
+  /*
+  lcd.setCursor(0,1);
+  lcd.print(line2);
+  */
+  
+  delay(100);
 }
